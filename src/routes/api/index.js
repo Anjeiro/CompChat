78f9852703ab3991988e8eb75be33c4b241f3ec8
Anjeiro <banguis.edward1@gmail.com>
2026-05-07 09:15:33 +0800

    Update index.js

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,2 +1,5 @@
-import handler from "../dist/server/server.js";
-export default handler;
+// This file must exist in the repo before build for Vercel to recognize it
+export default async function handler(req, res) {
+  const { default: serverHandler } = await import("../dist/server/server.js");
+  return serverHandler(req, res);
+}
