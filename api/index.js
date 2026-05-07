17b5176fe1a87ce23d50bcdb58cab1777513bd98
Anjeiro <banguis.edward1@gmail.com>
2026-05-07 09:37:23 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,11 @@
+import { join } from "path";
+import { pathToFileURL } from "url";
+import { createRequire } from "module";
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
 export default async function handler(req, res) {
-  const { default: serverHandler } = await import("../dist/server/server.js");
+  const serverPath = pathToFileURL(join(__dirname, "../dist/server/server.js")).href;
+  const { default: serverHandler } = await import(serverPath);
   return serverHandler(req, res);
 }
